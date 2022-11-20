2dd99c26e2a11fcea43f7761f17cfd6a8c297538
Miguel <miguelandradebarreto2@gmail.com>
2022-11-20 13:58:46 -0300

    :bug: solve local error bug

--- src/shared/database/index.ts
+++ src/shared/database/index.ts
@@ -1,12 +1,14 @@
 import { Pool } from 'pg'
 
+console.log(process.env.DB_HOST)
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
-  ssl: { rejectUnauthorized: false }
+  ssl: false
 })
 
 const query = async <T>(text: string) => pool.query<T>(text)
